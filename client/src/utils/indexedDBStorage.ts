386be579377b413728de7155018b34e74a6eddd7
William Munger <wcmunger@gmail.com>
2025-08-06 15:32:56 -0400

    Fix IndexedDB version mismatch

--- client/src/utils/indexedDBStorage.ts
+++ client/src/utils/indexedDBStorage.ts
@@ -14,7 +14,7 @@ export interface BackupData {
 
 export class IndexedDBStorage {
   private static readonly DB_NAME = 'ExpenseTrackerDB';
-  private static readonly DB_VERSION = 1;
+  private static readonly DB_VERSION = 3;
   private static readonly STORES = {
     users: 'users',
     categories: 'categories',
