5ab495f707d8aa8c411fc75f8f2ec771918434b8
William Munger <wcmunger@gmail.com>
2025-08-13 16:50:29 -0400

    Fix duplicate prop and syntax error in Settings component

--- client/src/components/Settings.tsx
+++ client/src/components/Settings.tsx
@@ -32,7 +32,6 @@ export const Settings: React.FC<SettingsProps> = ({
   onUpdateCredentials,
   currentCredentials,
  currentUseCase = 'family-expenses',
-  currentUseCase = 'family-expenses'
   authSettings,
   onUpdateAuthSettings
 }) => {
