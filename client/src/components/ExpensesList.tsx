cde573020ca8e6fc91d858879499e372f3210cc7
William Munger <wcmunger@gmail.com>
2025-08-15 14:32:38 -0400

    Updated ExpensesList.tsx

--- client/src/components/ExpensesList.tsx
+++ client/src/components/ExpensesList.tsx
@@ -470,7 +470,7 @@ export const ExpensesList: React.FC<ExpensesListProps> = ({
       <div className="bg-white rounded-2xl border border-slate-200 p-6">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-6 gap-1">
           <div className="relative xl:col-span-2">
-            <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-3 text-slate-400" />
+            <Search className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-slate-400" />
             <input
               type="text"
               placeholder="Search expenses, stores..."
@@ -481,7 +481,7 @@ export const ExpensesList: React.FC<ExpensesListProps> = ({
           </div>
 
           <div className="relative">
-            <Users className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-5 text-slate-400" />
+            <Users className="absolute left-3 top-1/2 -translate-y-1/2 w-5 h-1 text-slate-400" />
             <select
               value={filterUserId}
               onChange={(e) => setFilterUserId(e.target.value)}
